fb655333d84281986d0f5a2abc89eeb1fd44d5aa
Ivan Kalinin <k.beaver.i03@mail.ru>
2023-05-15 00:27:48 +0300

    fix

--- ZedGraphs/Form1.Designer.cs
+++ ZedGraphs/Form1.Designer.cs
@@ -30,11 +30,22 @@
         {
             this.components = new System.ComponentModel.Container();
             this.zedGraph = new ZedGraph.ZedGraphControl();
+            this.color_choice = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox_R = new System.Windows.Forms.TextBox();
+            this.textBox_a = new System.Windows.Forms.TextBox();
+            this.textBox_Rez = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox_Imz = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // zedGraph
             // 
-            this.zedGraph.Location = new System.Drawing.Point(13, 13);
+            this.zedGraph.Location = new System.Drawing.Point(0, 0);
+            this.zedGraph.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.zedGraph.Name = "zedGraph";
             this.zedGraph.ScrollGrace = 0D;
             this.zedGraph.ScrollMaxX = 0D;
@@ -43,21 +54,127 @@
             this.zedGraph.ScrollMinX = 0D;
             this.zedGraph.ScrollMinY = 0D;
             this.zedGraph.ScrollMinY2 = 0D;
-            this.zedGraph.Size = new System.Drawing.Size(747, 498);
+            this.zedGraph.Size = new System.Drawing.Size(998, 592);
             this.zedGraph.TabIndex = 0;
             this.zedGraph.UseExtendedPrintDialog = true;
             // 
+            // color_choice
+            // 
+            this.color_choice.AutoSize = true;
+            this.color_choice.Location = new System.Drawing.Point(1029, 71);
+            this.color_choice.Name = "color_choice";
+            this.color_choice.Size = new System.Drawing.Size(131, 20);
+            this.color_choice.TabIndex = 1;
+            this.color_choice.Text = "График в цвете";
+            this.color_choice.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1029, 456);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 136);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Построить График";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox_R
+            // 
+            this.textBox_R.Location = new System.Drawing.Point(1060, 115);
+            this.textBox_R.Name = "textBox_R";
+            this.textBox_R.Size = new System.Drawing.Size(100, 22);
+            this.textBox_R.TabIndex = 3;
+            // 
+            // textBox_a
+            // 
+            this.textBox_a.Location = new System.Drawing.Point(1060, 143);
+            this.textBox_a.Name = "textBox_a";
+            this.textBox_a.Size = new System.Drawing.Size(100, 22);
+            this.textBox_a.TabIndex = 4;
+            // 
+            // textBox_Rez
+            // 
+            this.textBox_Rez.Location = new System.Drawing.Point(1060, 171);
+            this.textBox_Rez.Name = "textBox_Rez";
+            this.textBox_Rez.Size = new System.Drawing.Size(100, 22);
+            this.textBox_Rez.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1024, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "R = ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1026, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "a = ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1005, 171);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Re z0 = ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1009, 205);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Im z0 = ";
+            // 
+            // textBox_Imz
+            // 
+            this.textBox_Imz.Location = new System.Drawing.Point(1060, 199);
+            this.textBox_Imz.Name = "textBox_Imz";
+            this.textBox_Imz.Size = new System.Drawing.Size(100, 22);
+            this.textBox_Imz.TabIndex = 10;
+            this.textBox_Imz.TextChanged += new System.EventHandler(this.textBox_Imz_TextChanged);
+            // 
             // Form1
             // 
-            this.ClientSize = new System.Drawing.Size(1190, 640);
+            this.ClientSize = new System.Drawing.Size(1190, 595);
+            this.Controls.Add(this.textBox_Imz);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_Rez);
+            this.Controls.Add(this.textBox_a);
+            this.Controls.Add(this.textBox_R);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.color_choice);
             this.Controls.Add(this.zedGraph);
             this.Name = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private ZedGraph.ZedGraphControl zedGraph;
+        private System.Windows.Forms.CheckBox color_choice;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox_R;
+        private System.Windows.Forms.TextBox textBox_a;
+        private System.Windows.Forms.TextBox textBox_Rez;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox_Imz;
     }
 }
--- ZedGraphs/Form1.cs
+++ ZedGraphs/Form1.cs
@@ -8,55 +8,57 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using ZedGraph;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
 
 namespace ZedGraphs
 {
     public partial class Form1 : Form
     {
         int max_iter = 100;
-        double a1 = -0.5;
-        double a2 = 3;
+        double a = 3;
         double R = 10;
         double Re_c;
         double Im_c;
+        double Im_z = 0;
+        double Re_z = 0;
         PointPairList list = new PointPairList();
         PointPairList list2 = new PointPairList();
         PointPairList list3 = new PointPairList();
         PointPairList list4 = new PointPairList();
         PointPairList list5 = new PointPairList();
         PointPairList list6 = new PointPairList();
+        LineItem myCurve2;
+        LineItem myCurve3;
+        LineItem myCurve4;
+        LineItem myCurve5;
+        LineItem myCurve6;
 
         public Form1()
         {
             InitializeComponent();
-            DrawGraph();
         }
 
         private void Maldenbrot()
         {
-            double Im_z;
-            double Re_z;
+            double Im_z2 = Im_z;
+            double Re_z2 = Re_z;
             double Im_z_copy;
             double Re_z_copy;
             double distans;
-            for (double i = a1; i < a2; i += 0.005)
+            for (double i = -a; i < a; i += 0.005)
             {
-                for (double j = a1; j < a2; j += 0.005)
+                for (double j = -a; j < a; j += 0.005)
                 {
                     bool flag = false;
-                    Im_z = 0;
-                    Re_z = 0;
+                    Im_z2 = 0;
+                    Re_z2 = 0;
                     Im_c = j;
                     Re_c = i;
                     for (int count = 0; count < max_iter; count++)
                     {
-                        Re_z_copy = Re_z - Re_z* Re_z + Im_z * Im_z + Re_c;
-                        Im_z_copy = Im_z* Re_z + Im_c;
+                        Re_z_copy = Re_z2 - Re_z2* Re_z2 + Im_z2 * Im_z2 + Re_c;
+                        Im_z_copy = Im_z2* Re_z2 + Im_c;
                         distans = Math.Sqrt(Math.Pow(Re_z_copy, 2) + Math.Pow(Im_z_copy, 2));
-                        if (i == 0 && j == 0)
-                        {
-                            Console.WriteLine(count);
-                        }
                         if (distans > R)
                         {
                             flag = true;
@@ -82,8 +84,8 @@ namespace ZedGraphs
                             }
                             break;
                         }
-                        Re_z = Re_z_copy;
-                        Im_z = Im_z_copy;
+                        Re_z2 = Re_z_copy;
+                        Im_z2 = Im_z_copy;
 
                     }
                     if (flag == false)
@@ -96,6 +98,12 @@ namespace ZedGraphs
         private void DrawGraph()
         {
             GraphPane pane = zedGraph.GraphPane;
+            list.Clear();
+            list2.Clear();
+            list3.Clear();
+            list4.Clear();
+            list5.Clear();
+            list6.Clear();
 
             // Очистим список кривых
             pane.CurveList.Clear();
@@ -103,43 +111,69 @@ namespace ZedGraphs
 
             double xmin = -0.5;
             double xmax = 3;
-
+            if (textBox_a.Text != "")
+            {
+                a = Convert.ToDouble(textBox_a.Text);
+            }
+            if (textBox_R.Text != "")
+            {
+                R = Convert.ToDouble(textBox_a.Text);
+            }
+            if (textBox_Rez.Text != "")
+            {
+                Re_z = Convert.ToDouble(textBox_a.Text);
+            }
+            if (textBox_Imz.Text != "")
+            {
+                Im_z = Convert.ToDouble(textBox_a.Text);
+            }
             // Заполняем список точек
             Maldenbrot();
             LineItem myCurve = pane.AddCurve("Scatter", list, Color.Black, SymbolType.Circle);
-            LineItem myCurve2 = pane.AddCurve("Scatter", list2, Color.Red, SymbolType.Circle);
-            LineItem myCurve3 = pane.AddCurve("Scatter", list3, Color.Green, SymbolType.Circle);
-            LineItem myCurve4 = pane.AddCurve("Scatter", list4, Color.Gray, SymbolType.Circle);
-            LineItem myCurve5 = pane.AddCurve("Scatter", list5, Color.Khaki, SymbolType.Circle);
-            LineItem myCurve6 = pane.AddCurve("Scatter", list6, Color.Lime, SymbolType.Circle);
+            if (color_choice.Checked == true)
+            {
+                myCurve2 = pane.AddCurve("< 5", list2, Color.Red, SymbolType.Circle);
+                myCurve3 = pane.AddCurve("< 10", list3, Color.Green, SymbolType.Circle);
+                myCurve4 = pane.AddCurve("< 15", list4, Color.Gray, SymbolType.Circle);
+                myCurve5 = pane.AddCurve("< 30", list5, Color.Khaki, SymbolType.Circle);
+                myCurve6 = pane.AddCurve("> 30", list6, Color.Lime, SymbolType.Circle);
+            }
 
             // !!!
             // У кривой линия будет невидимой
             myCurve.Line.IsVisible = false;
-            myCurve2.Line.IsVisible = false;
-            myCurve3.Line.IsVisible = false;
-            myCurve4.Line.IsVisible = false;
-            myCurve5.Line.IsVisible = false;
-            myCurve6.Line.IsVisible = false;
+            if (color_choice.Checked == true)
+            {
+                myCurve2.Line.IsVisible = false;
+                myCurve3.Line.IsVisible = false;
+                myCurve4.Line.IsVisible = false;
+                myCurve5.Line.IsVisible = false;
+                myCurve6.Line.IsVisible = false;
+            }
 
             // !!!
             // Цвет заполнения отметок (ромбиков) - голубой
             myCurve.Symbol.Fill.Color = Color.Black;
-            myCurve2.Symbol.Fill.Color = Color.Red;
-            myCurve3.Symbol.Fill.Color = Color.Green;
-            myCurve4.Symbol.Fill.Color = Color.Gray;
-            myCurve5.Symbol.Fill.Color = Color.Khaki;
-            myCurve6.Symbol.Fill.Color = Color.Lime;
+            if (color_choice.Checked == true)
+            {
+                myCurve2.Symbol.Fill.Color = Color.Red;
+                myCurve3.Symbol.Fill.Color = Color.Green;
+                myCurve4.Symbol.Fill.Color = Color.Gray;
+                myCurve5.Symbol.Fill.Color = Color.Khaki;
+                myCurve6.Symbol.Fill.Color = Color.Lime;
+            }
 
             // !!!
             // Тип заполнения - сплошная заливка
             myCurve.Symbol.Fill.Type = FillType.Solid;
-            myCurve2.Symbol.Fill.Type = FillType.Solid;
-            myCurve3.Symbol.Fill.Type = FillType.Solid;
-            myCurve4.Symbol.Fill.Type = FillType.Solid;
-            myCurve5.Symbol.Fill.Type = FillType.Solid;
-            myCurve6.Symbol.Fill.Type = FillType.Solid;
-
+            if (color_choice.Checked == true)
+            {
+                myCurve2.Symbol.Fill.Type = FillType.Solid;
+                myCurve3.Symbol.Fill.Type = FillType.Solid;
+                myCurve4.Symbol.Fill.Type = FillType.Solid;
+                myCurve5.Symbol.Fill.Type = FillType.Solid;
+                myCurve6.Symbol.Fill.Type = FillType.Solid;
+            }
             // !!!
             // Размер ромбиков
             myCurve.Symbol.Size = 7;
@@ -150,16 +184,35 @@ namespace ZedGraphs
             pane.XAxis.Scale.Max = xmax;
 
             // Устанавливаем интересующий нас интервал по оси Y
-            pane.YAxis.Scale.Min = -0.5;
-            pane.YAxis.Scale.Max = 1;
+            pane.YAxis.Scale.Min = -2;
+            pane.YAxis.Scale.Max = 2;
 
             // Вызываем метод AxisChange (), чтобы обновить данные об осях.
             // В противном случае на рисунке будет показана только часть графика,
             // которая умещается в интервалы по осям, установленные по умолчанию
             zedGraph.AxisChange();
-
             // Обновляем график
             zedGraph.Invalidate();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DrawGraph();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void textBox_Imz_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
